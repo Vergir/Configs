e8bde7d00af3bd06eb5e780d2e8ee6df18eb2403
Daniil Popov <dnlppv@n0404.mashtab.org>
2017-10-16 12:31:41 +0000

    Symbolic links override old ones if they are present

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ if [ -f ~/.inputrc ];
 then
 	[ -h ~/.inputrc ] || echo "\$include ${CONFIGS_DIR}/.inputrc" >> ~/.inputrc
 else
-	ln -s ${CONFIGS_DIR}/.inputrc ~/.inputrc
+	ln -sf ${CONFIGS_DIR}/.inputrc ~/.inputrc
 fi
 
 echo "APPLYING .bashrc ..."
@@ -15,7 +15,7 @@ if [ -f ~/.bashrc ];
 then
 	[ -h ~/.bashrc ] || echo "source ${CONFIGS_DIR}/.bashrc" >> ~/.bashrc
 else
-	ln -s ${CONFIGS_DIR}/.bashrc ~/.bashrc
+	ln -sf ${CONFIGS_DIR}/.bashrc ~/.bashrc
 fi
 
 echo "APPLYING .vimrc ..."
@@ -23,7 +23,7 @@ if [ -f ~/.vimrc ];
 then
 	[ -h ~/.vimrc ] || echo "source ${CONFIGS_DIR}/.vimrc" >> ~/.vimrc
 else
-	ln -s ${CONFIGS_DIR}/.vimrc ~/.vimrc
+	ln -sf ${CONFIGS_DIR}/.vimrc ~/.vimrc
 fi
 
 if [ -d ~/.vim ];
@@ -31,7 +31,7 @@ then
 	echo "~/.vim ALREADY PRESENT. MERGE MANUALLY"
 else
 	echo "APPLYING ~/.vim"
-	ln -s ${CONFIGS_DIR}/.vim ~/.vim
+	ln -sf ${CONFIGS_DIR}/.vim ~/.vim
 fi
 
 if [ -d ~/.fonts ];
@@ -39,7 +39,7 @@ then
 	echo "~/.fonts ALREADY PRESENT. MERGE MANUALLY"
 else
 	echo "APPLYING ~/.fonts"
-	ln -s ${CONFIGS_DIR}/fonts ~/.fonts
+	ln -sf ${CONFIGS_DIR}/fonts ~/.fonts
 fi
 
 echo -n "WE XTERM? ... "
@@ -47,7 +47,7 @@ if [[ $TERM == *"xterm"* ]];
 then
 	echo "YES. APPLYING .Xresources"
 	[ -f ~/.Xresources ] && mv ~/.Xresources ~/.Xresources.old
-	ln -s ${CONFIGS_DIR}/.Xresources ~/.Xresources
+	ln -sf ${CONFIGS_DIR}/.Xresources ~/.Xresources
 else
 	echo "NO."
 fi
@@ -57,7 +57,7 @@ if ! [ -z "$(type -t git)" ];
 then
 	echo "YES. APPLYING .gitconfig"
 	[ -f ~/.gitconfig ] && mv ~/.gitconfig ~/.gitconfig.old
-	ln -s ${CONFIGS_DIR}/.gitconfig ~/.gitconfig
+	ln -sf ${CONFIGS_DIR}/.gitconfig ~/.gitconfig
 else
 	echo "NO."
 fi
